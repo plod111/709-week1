028f9836495658a0dcd2d5fb6b5867b3738ce6e0
plod111 <ben.cornish@gmail.com>
2023-08-31 21:43:43 -0400

    Final version

---
+++ Date.java
@@ -0,0 +1,29 @@
+public class Date{
+/* BCornish -  CSCI 70900 */
+/* Hunter CUNY Aug 2023 */
+/* EXERCISE 2, Chapter 2 - Think Java 2 */
+
+    public static void main(String[] args){
+
+        System.out.println("Chapter 2, Ex 2 - ThinkJava2");
+
+        String day = "Thursday";
+        int date = 31;
+        String month = "August";
+        int year = 2023;
+
+        /** System.out.println("Day of week = " + day);
+        System.out.println("Date of month = " + date);
+        System.out.println("Month of year = " + month);
+        System.out.println("Year = " + year);
+        **/
+
+        System.out.println();
+
+        System.out.println("American format: " + day + ", " + month + ", " + date + ", " + year);
+
+        System.out.println("European format: " + day + " " + date + " " + month + " " + year);
+
+    }
+
+}
